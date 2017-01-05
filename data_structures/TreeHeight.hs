54bbc0ca4369ff648968d513cf21c30458871ec9
akkhong <anthony.kusumo.khong@gmail.com>
2017-01-05 12:58:22 +0700

    Mutable arrays

--- data_structures/TreeHeight.hs
+++ data_structures/TreeHeight.hs
@@ -1,8 +1,13 @@
-import           Control.Monad.ST
-import qualified Data.Array       as A
-import qualified Data.Array.ST    as STA
+import           Control.Monad
+import qualified Data.Array    as A
+import qualified Data.Array.ST as STA
 import           Data.List
-import qualified Data.Sequence    as S
+import qualified Data.Sequence as S
+
+type Arr a = A.Array Int a
+type Seq a = S.Seq a
+type Table = Arr (Seq Int)
+data Tree a = Node a (Seq (Tree a)) deriving (Show)
 
 main :: IO ()
 main = do
@@ -14,15 +19,18 @@ main = do
 parseInts :: String -> [Int]
 parseInts = map read . words
 
-type Arr a = A.Array Int a
-type Seq a = S.Seq a
-type Table = Arr (Seq Int)
-type MutableTable s = ST s (STA.STArray s Int (Seq Int))
-data Tree a = Node a (Seq (Tree a)) deriving (Show)
+toArray :: [a] -> Arr a
+toArray xs = A.listArray (0, length xs - 1) xs
+
+aLength :: Arr a -> Int
+aLength = (+1) . negate . uncurry (-) . A.bounds
+
+sMaximum :: (Ord a, Bounded a) => Seq a -> a
+sMaximum = S.foldlWithIndex (\x _ y -> max x y) minBound
 
 fromList :: [Int] -> Tree Int
 fromList xs = fromList' root table
-    where table = makeLookUpTable xs
+    where table = makeLookUpTable . toArray $ xs
           (Just root) = elemIndex (-1) xs
 
 fromList' :: Int -> Table -> Tree Int
@@ -32,26 +40,22 @@ fromList' root table = subtree root
 height :: Tree a -> Int
 height (Node _ ts)
   | S.null ts = 1
-  | otherwise = 1 + maximum (fmap height ts)
+  | otherwise = 1 + sMaximum (fmap height ts)
 
-makeLookUpTable :: [Int] -> Table
-makeLookUpTable xs = STA.runSTArray $ foldl addGroup initTable zipped
-    where initTable = emptyTable (length xs)
-          zipped = zip [0..] xs
-
-emptyTable :: Int -> MutableTable s
-emptyTable n = STA.newArray (0, n - 1) S.empty
-
-addGroup :: MutableTable s -> (Int, Int) -> MutableTable s
-addGroup xs (childIx, parentIx) = do
-    xs' <- xs
-    return xs'
-
-newArr = STA.runSTArray $ do
-    xs <- STA.newArray (0, 10) 3 :: ST s (STA.STArray s Int Int)
-    return xs
+makeLookUpTable :: Arr Int -> Table
+makeLookUpTable xs = STA.runSTArray $ do
+    let n = aLength xs
+    table <- STA.newArray (0, n-1) S.empty
+    forM_ [0..(n-1)] $ \i ->
+        if xs A.! i == (-1)
+           then return ()
+           else do
+               let parentIx = xs A.! i
+               children <- STA.readArray table parentIx
+               STA.writeArray table parentIx (children S.|> i)
+    return table
 
 {-
-ixs = [4,-1,4,1,1]
-ixs = [8, 8, 5, 6, 7, 3, 1, 6, -1, 5]
+xs = [4,-1,4,1,1]
+xs = [8, 8, 5, 6, 7, 3, 1, 6, -1, 5]
 -}
