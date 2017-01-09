461eff8b162b24aa5d24753cc6bd96b07e9b68d2
akkhong <anthony.kusumo.khong@gmail.com>
2017-01-09 19:23:13 +0700

    .

--- data_structures/ConvertArrayToHeap.hs
+++ data_structures/ConvertArrayToHeap.hs
@@ -1,31 +1,29 @@
 import           Control.Monad
-import           Control.Monad.ST
-import qualified Data.Array       as A
-import qualified Data.Array.ST    as STA
+import qualified Data.Array    as A
+import qualified Data.Array.IO as MA
 import           Data.List
 
 type Arr a = A.Array Int a
-type BSTArr s a = ST s (STA.STArray s Int a)
-type STArr s a = STA.STArray s Int a
-
-{-TODO: swap ST Array to IO Array-}
+type MArr a = MA.IOArray Int a
+type BMArr a = IO (MArr a)
 
 main :: IO ()
 main = do
     _ <- getLine
     xs <- getInts
-    let heap = buildHeap xs
-    print heap
+    heap <- buildHeap xs
+    hs <- mapM (MA.readArray heap) [0..4]
+    print hs
 
 getInts :: IO [Int]
 getInts = getLine >>= return . map read . words
 
-buildHeap :: [Int] -> Arr Int
-buildHeap xs = STA.runSTArray (buildHeap' arr initIter)
+buildHeap :: [Int] -> BMArr Int
+buildHeap xs = buildHeap' arr initIter
     where arr = toSTArr . toArray $ xs
           initIter = (length xs `div` 2) - 1
 
-buildHeap' :: BSTArr s Int -> Int -> BSTArr s Int
+buildHeap' :: BMArr Int -> Int -> BMArr Int
 buildHeap' xs i =
     if i < 0
        then xs
@@ -40,40 +38,37 @@ left = oneToZero (*2)
 right :: Int -> Int
 right = oneToZero $ (+1) . (*2)
 
-siftDown :: Int -> BSTArr s Int -> BSTArr s Int
+siftDown :: Int -> BMArr Int -> BMArr Int
 siftDown i xs = do
     j <- xs >>= getSwapIx [i, left i, right i]
     if i == j
        then xs
        else siftDown j (xs >>= swapIndex i j)
 
-getSwapIx :: [Int] -> STArr s Int -> ST s Int
+getSwapIx :: [Int] -> MArr Int -> IO Int
 getSwapIx ixs xs = do
-    n <- STA.getBounds xs >>= boundsToLength
+    n <- MA.getBounds xs >>= boundsToLength
     vs <- mapM (safeRead xs n) ixs
     return (swapIx vs ixs)
 
-safeRead :: STArr s Int -> Int -> Int -> ST s Int
-safeRead xs n i = if i < n then STA.readArray xs i else return maxBound
+safeRead :: MArr Int -> Int -> Int -> IO Int
+safeRead xs n i = if i < n then MA.readArray xs i else return maxBound
 
 swapIx :: [Int] -> [Int] -> Int
 swapIx xs ixs = snd . head . sort $ zip xs ixs
 
-swapIndex :: Int -> Int -> STArr s Int -> BSTArr s Int
-swapIndex i j xs =
-    if i == j
-       then return xs
-       else do
-           [xi, xj] <- mapM (STA.readArray xs) [i, j]
-           STA.writeArray xs i xj
-           STA.writeArray xs j xi
-           return xs
+swapIndex :: Int -> Int -> MArr Int -> BMArr Int
+swapIndex i j xs = do
+    [xi, xj] <- mapM (MA.readArray xs) [i, j]
+    MA.writeArray xs i xj
+    MA.writeArray xs j xi
+    return xs
 
-toSTArr :: Arr Int -> BSTArr s Int
+toSTArr :: Arr Int -> BMArr Int
 toSTArr xs = do
     n <- boundsToLength (A.bounds xs)
-    xs' <- STA.newArray (0, n-1) 0
-    forM_ [0..(n-1)] $ \i -> STA.writeArray xs' i (xs A.! i)
+    xs' <- MA.newArray (0, n-1) 0
+    forM_ [0..(n-1)] $ \i -> MA.writeArray xs' i (xs A.! i)
     return xs'
 
 toArray :: [a] -> Arr a
