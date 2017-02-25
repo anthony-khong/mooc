9b37ff02d5831c8e5636ae6f33bc0e591a47a597
akkhong <anthony.kusumo.khong@gmail.com>
2017-02-25 12:46:36 +0700

    Added type aliases

--- algorithms_on_graphs/dijkstra.cpp
+++ algorithms_on_graphs/dijkstra.cpp
@@ -7,6 +7,7 @@ struct Edge {
     int key;
     int weight;
 };
+using EdgeVector = std::vector<Edge>;
 
 Edge create_edge(int key, int weight) {
     Edge edge;
@@ -21,6 +22,7 @@ class Vertex {
         std::vector<Edge> incoming;
         std::vector<Edge> outgoing;
 };
+using VertexMap = std::map<int,Vertex>;
 
 Vertex create_singleton_vertex(int key) {
     Vertex vertex;
@@ -57,14 +59,20 @@ std::string to_str(Vertex vertex) {
     std::string key_str = to_str(vertex.key);
     std::string incoming_str = vector_to_str(vertex.incoming);
     std::string outgoing_str = vector_to_str(vertex.outgoing);
-
     return "V(" + key_str + ")(in=" + incoming_str + ", out=" + outgoing_str + ")";
 }
 
-std::map<int,Vertex> parse_vertices() {
+template <typename K, typename V>
+void print_map(std::map<K,V> map) {
+    for (auto& kv: map) {
+        std::cout << to_str(kv.first) << ": " << to_str(kv.second) << '\n';
+    }
+}
+
+VertexMap parse_vertices() {
     int n_vertices, n_edges;
     std::cin >> n_vertices >> n_edges;
-    std::map<int,Vertex> vertices;
+    VertexMap vertices;
     for (int i = 0; i < n_vertices; ++i) {
         vertices[i] = create_singleton_vertex(i + 1);
     }
@@ -77,44 +85,17 @@ std::map<int,Vertex> parse_vertices() {
     return vertices;
 }
 
-int main() {
-    std::map<int,Vertex> vertices = parse_vertices();
-    for (auto& kv: vertices) {
-        int key = kv.first;
-        Vertex vertex = kv.second;
-        std::cout << key << ": " << to_str(vertex) << std::endl;
-    }
-}
-
-/*
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using std::vector;
-using std::queue;
-using std::pair;
-using std::priority_queue;
-
-int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
-}
+//int min_cost(VertexMap vertices, int start, int end) {
+    //return 10;
+//}
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  int s, t;
-  std::cin >> s >> t;
-  s--, t--;
-  std::cout << distance(adj, cost, s, t);
+    VertexMap vertices = parse_vertices();
+    print_map(vertices);
+
+    //int start, end;
+    //std::cin >> start >> end;
+    //int result = min_cost(vertices, start, end);
+    //std::cout << result;
+
 }
-*/
