391b89077ab49c4d8a63bf3d15cbad01c66ec7b3
akkhong <anthony.kusumo.khong@gmail.com>
2017-06-27 12:36:13 +0700

    .

--- fp_in_scala/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ fp_in_scala/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -5,6 +5,7 @@ object Anagrams {
   type Word = String
   type Sentence = List[Word]
   type Occurrences = List[(Char, Int)]
+  type Dictionary = Map[Occurrences, List[Word]]
 
   val dictionary: List[Word] = loadDictionary
 
@@ -43,57 +44,29 @@ object Anagrams {
     }
   }
 
-  /** Subtracts occurrence list `y` from occurrence list `x`.
-   *
-   *  The precondition is that the occurrence list `y` is a subset of
-   *  the occurrence list `x` -- any character appearing in `y` must
-   *  appear in `x`, and its frequency in `y` must be smaller or equal
-   *  than its frequency in `x`.
-   *
-   *  Note: the resulting value is an occurrence - meaning it is sorted
-   *  and has no zero-entries.
-   */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+    def substractOcc(map: Map[Char, Int], occ: (Char, Int)): Map[Char, Int] = {
+      val (ltr, n) = occ
+      val updatedCount = map(ltr) - n
+      if (updatedCount == 0) map - ltr else map + (ltr -> updatedCount)
+    }
+    val occMap = x.toMap withDefaultValue 0
+    val substractedMap = (y foldLeft occMap)(substractOcc)
+    substractedMap.toList.sorted
+    }
 
-  /** Returns a list of all anagram sentences of the given sentence.
-   *
-   *  An anagram of a sentence is formed by taking the occurrences of all the characters of
-   *  all the words in the sentence, and producing all possible combinations of words with those characters,
-   *  such that the words have to be from the dictionary.
-   *
-   *  The number of words in the sentence and its anagrams does not have to correspond.
-   *  For example, the sentence `List("I", "love", "you")` is an anagram of the sentence `List("You", "olive")`.
-   *
-   *  Also, two sentences with the same words but in a different order are considered two different anagrams.
-   *  For example, sentences `List("You", "olive")` and `List("olive", "you")` are different anagrams of
-   *  `List("I", "love", "you")`.
-   *
-   *  Here is a full example of a sentence `List("Yes", "man")` and its anagrams for our dictionary:
-   *
-   *    List(
-   *      List(en, as, my),
-   *      List(en, my, as),
-   *      List(man, yes),
-   *      List(men, say),
-   *      List(as, en, my),
-   *      List(as, my, en),
-   *      List(sane, my),
-   *      List(Sean, my),
-   *      List(my, en, as),
-   *      List(my, as, en),
-   *      List(my, sane),
-   *      List(my, Sean),
-   *      List(say, men),
-   *      List(yes, man)
-   *    )
-   *
-   *  The different sentences do not have to be output in the order shown above - any order is fine as long as
-   *  all the anagrams are there. Every returned word has to exist in the dictionary.
-   *
-   *  Note: in case that the words of the sentence are in the dictionary, then the sentence is the anagram of itself,
-   *  so it has to be returned in this list.
-   *
-   *  Note: There is only one anagram of an empty sentence.
-   */
-  def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+  def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
+    val sentenceOccs = sentenceOccurrences(sentence)
+    val dict = dictionaryByOccurrences withDefaultValue List()
+    occAnagrams(sentenceOccs, dict)
+  }
+
+  def occAnagrams(sentenceOccs: Occurrences, dict: Dictionary): List[Sentence] = {
+    if (sentenceOccs.isEmpty) List(List())
+    else for {
+      wordOccs <- combinations(sentenceOccs)
+      word <- dict(wordOccs)
+      otherWords <- occAnagrams(subtract(sentenceOccs, wordOccs), dict)
+    } yield word :: otherWords
+  }
 }
