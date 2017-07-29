0f6034c7143022c0857d3229e3f71b8e76e75b34
akkhong <anthony.kusumo.khong@gmail.com>
2017-07-29 19:40:39 +0700

    .

---
+++ parallel_programming/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -0,0 +1,100 @@
+package reductions
+
+import org.scalameter._
+import common._
+
+object ParallelCountChangeRunner {
+
+  @volatile var seqResult = 0
+
+  @volatile var parResult = 0
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 40,
+    Key.exec.benchRuns -> 80,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val amount = 250
+    val coins = List(1, 2, 5, 10, 20, 50)
+    val seqtime = standardConfig measure {
+      seqResult = ParallelCountChange.countChange(amount, coins)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential count time: $seqtime ms")
+
+    def measureParallelCountChange(threshold: ParallelCountChange.Threshold): Unit = {
+      val fjtime = standardConfig measure {
+        parResult = ParallelCountChange.parCountChange(amount, coins, threshold)
+      }
+      println(s"parallel result = $parResult")
+      println(s"parallel count time: $fjtime ms")
+      println(s"speedup: ${seqtime / fjtime}")
+    }
+
+    measureParallelCountChange(ParallelCountChange.moneyThreshold(amount))
+    measureParallelCountChange(ParallelCountChange.totalCoinsThreshold(coins.length))
+    measureParallelCountChange(ParallelCountChange.combinedThreshold(amount, coins))
+  }
+}
+
+object ParallelCountChange {
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    (money, coins) match {
+      case (0, _) => 1
+      case (_, Nil) => 0
+      case (m, _) if (m < 0) => 0
+      case (m, coin :: tailCoins) => {
+        val countCoinUsed = countChange(m - coin, coin :: tailCoins)
+        val countCoinNotUsed = countChange(m, tailCoins)
+        countCoinUsed + countCoinNotUsed
+      }
+    }
+  }
+
+  type Threshold = (Int, List[Int]) => Boolean
+
+  /** In parallel, counts the number of ways change can be made from the
+   *  specified list of coins for the specified amount of money.
+   */
+  def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
+    if (threshold(money, coins)) countChange(money, coins)
+    else {
+      (money, coins) match {
+        case (0, _) => 1
+        case (_, Nil) => 0
+        case (m, _) if (m < 0) => 0
+        case (m, coin :: tailCoins) => {
+          val (countUsed, countNotUsed) = parallel(
+            parCountChange(m - coin, coins, threshold),
+            parCountChange(m, tailCoins, threshold)
+          )
+          countUsed + countNotUsed
+        }
+      }
+    }
+  }
+
+  /** Threshold heuristic based on the starting money. */
+  def moneyThreshold(startingMoney: Int): Threshold = {
+    case (money, coins)  => money <= (2.0 * startingMoney / 3.0)
+  }
+
+  /** Threshold heuristic based on the total number of initial coins. */
+  def totalCoinsThreshold(totalCoins: Int): Threshold = {
+    case (money, coins) => coins.size <= (2.0 * totalCoins / 3.0)
+  }
+
+  /** Threshold heuristic based on the starting money and the initial list of coins. */
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
+    def threshold(money: Int, coins: List[Int]) = {
+      val checkMoney = moneyThreshold(startingMoney)
+      val checkCoins = totalCoinsThreshold(allCoins.size)
+      checkMoney(money, coins) | checkCoins(money, coins)
+    }
+    threshold
+  }
+}
---
+++ parallel_programming/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,92 @@
+package reductions
+
+import scala.annotation._
+import org.scalameter._
+import common._
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime ms")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime ms")
+    println(s"speedup: ${seqtime / fjtime}")
+  }
+}
+
+object ParallelParenthesesBalancing {
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+    def countBrace(count: Int, char: Char): Int = char match {
+      case '(' => count + 1
+      case ')' => count - 1
+      case _ => count
+    }
+    val braceCounts = chars.scanLeft(0)(countBrace)
+    val negativeCounts = braceCounts filter (_<0)
+    (negativeCounts.size == 0) & (braceCounts.last == 0)
+  }
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    //def traverse(from: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+    def traverse(from: Int, until: Int): (Int, Int) = {
+      var left = from
+      var right = until
+      var count, min = 0
+      while (left < right) {
+        if (chars(left) == '(')
+          count = count + 1
+        else if (chars(left) == ')')
+          count = count - 1
+        if (count < min)
+          min = count
+        left = left + 1
+      }
+      (count, min)
+    }
+
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from < threshold) {
+        traverse(from, until)
+      } else {
+        val mid = (from + until) / 2
+        val ((endLeft, minLeft), (endRight, minRight)) = parallel(reduce(from, mid), reduce(mid, until))
+        val realMinRight = minRight + endLeft
+        (endLeft + endRight, if (realMinRight < minLeft) realMinRight else minLeft)
+      }
+    }
+
+    reduce(0, chars.length) == (0, 0)
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+}
